2b3183044d22512df489d7b49450730d624a5c3d
jonas pauli <jonaspauli089@gmail.com>
2024-09-16 16:38:30 +0200

    failing pairing test

--- tests/circom128.rs
+++ tests/circom128.rs
@@ -165,6 +165,10 @@ fn circom_multiplier_2() {
     */
     // ax, ay, bay, bax, bby, bbx
     let vk = compute_vk(ics, inputs);
+
+    let indermediate = alt_bn128_pairing(vec![(pi_a_x, pi_a_y, pi_b_x, pi_b_y, pi_b_x2, pi_b_y2)]);
+    println!("Indermediate: {}", indermediate);
+
     let result = alt_bn128_pairing(vec![
         (a_neg.0, a_neg.1, pi_b_y, pi_b_x, pi_b_y2, pi_b_x2),
         (
@@ -193,4 +197,91 @@ fn circom_multiplier_2() {
         ),
     ]);
     println!("Result: {}", &result);
+}
+
+#[test]
+fn test_pairing() {
+    let ax_1 = U256::from_str_radix(
+        "1c76476f4def4bb94541d57ebba1193381ffa7aa76ada664dd31c16024c43f59",
+        16,
+    )
+    .unwrap();
+    let ay_1 = U256::from_str_radix(
+        "3034dd2920f673e204fee2811c678745fc819b55d3e9d294e45c9b03a76aef41",
+        16,
+    )
+    .unwrap();
+    let bax_1 = U256::from_str_radix(
+        "209dd15ebff5d46c4bd888e51a93cf99a7329636c63514396b4a452003a35bf7",
+        16,
+    )
+    .unwrap();
+    let bay_1 = U256::from_str_radix(
+        "04bf11ca01483bfa8b34b43561848d28905960114c8ac04049af4b6315a41678",
+        16,
+    )
+    .unwrap();
+    let bbx_1 = U256::from_str_radix(
+        "2bb8324af6cfc93537a2ad1a445cfd0ca2a71acd7ac41fadbf933c2a51be344d",
+        16,
+    )
+    .unwrap();
+    let bby_1 = U256::from_str_radix(
+        "120a2a4cf30c1bf9845f20c6fe39e07ea2cce61f0c9bb048165fe5e4de877550",
+        16,
+    )
+    .unwrap();
+
+    let ax_2 = U256::from_str_radix(
+        "111e129f1cf1097710d41c4ac70fcdfa5ba2023c6ff1cbeac322de49d1b6df7c",
+        16,
+    )
+    .unwrap();
+    let ay_2 = U256::from_str_radix(
+        "2032c61a830e3c17286de9462bf242fca2883585b93870a73853face6a6bf411",
+        16,
+    )
+    .unwrap();
+    let bax_2 = U256::from_str_radix(
+        "198e9393920d483a7260bfb731fb5d25f1aa493335a9e71297e485b7aef312c2",
+        16,
+    )
+    .unwrap();
+    let bay_2 = U256::from_str_radix(
+        "1800deef121f1e76426a00665e5c4479674322d4f75edadd46debd5cd992f6ed",
+        16,
+    )
+    .unwrap();
+    let bbx_2 = U256::from_str_radix(
+        "090689d0585ff075ec9e99ad690c3395bc4b313370b38ef355acdadcd122975b",
+        16,
+    )
+    .unwrap();
+    let bby_2 = U256::from_str_radix(
+        "12c85ea5db8c6deb4aab71808dcb408fe3d1e7690c43d37b4ce6cc0166fa7daa",
+        16,
+    )
+    .unwrap();
+
+    dbg!(
+        ax_1.to_le_bytes(),
+        ay_1.to_le_bytes(),
+        bax_1.to_le_bytes(),
+        bay_1.to_le_bytes(),
+        bbx_1.to_le_bytes(),
+        bby_1.to_le_bytes(),
+        ax_2.to_le_bytes(),
+        ay_2.to_le_bytes(),
+        bax_2.to_le_bytes(),
+        bay_2.to_le_bytes(),
+        bbx_2.to_le_bytes(),
+        bby_2.to_le_bytes()
+    );
+
+    let result = alt_bn128_pairing(vec![
+        (ax_1, ay_1, bax_1, bay_1, bbx_1, bby_1),
+        (ax_2, ay_2, bax_2, bay_2, bbx_2, bby_2),
+    ]);
+    println!("Result: {}", &result);
+    assert!(result);
 }
